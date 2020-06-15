d3e85e2a96d703ac1791d37451a4bc9200b45779
Chris Bourke <cbourke@cse.unl.edu>
2020-06-15 14:59:23 -0500

    style updates

--- imageDriver.c
+++ imageDriver.c
@@ -1,4 +1,8 @@
-
+/**
+ * This is a main test driver program that allows you
+ * to test your functions on an actual image visually
+ * in an ad-hoc manner.  
+ */
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -8,6 +12,8 @@ int main(int argc, char **argv) {
 
   int h, w;
   Pixel **image = loadImage("yourImage.jpeg", &h, &w);
+  
+  //TODO: perform any function calls here
 
   saveImage("copy.jpg", image, h, w);
 
