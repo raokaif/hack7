f4f9d7d7d4081a945126facd678e801e7ee93a4d
raokaif <kaifrao250@gmail.com>
2023-12-20 23:00:28 +0500

    using enum

--- array_utils.c
+++ array_utils.c
@@ -1,17 +1,24 @@
 
 
 // place your function definitions here
+enum errorcode{
+    true,
+    false
+};
 int contains(const int *arr, int size, int x)
 {
     for (int i = 0; i < size; i++, arr++)
     {
         if (*arr == x)
         {
-            return 0;
+            return true;
         }
         else
         {
-            return 1;
+            return false;
         }
     }
+}
+int containsWithin(const int *arr, int size, int x, int i, int j){
+
 }
