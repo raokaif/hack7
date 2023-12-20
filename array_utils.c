3a6d08cefc102147d67f360aac08efbd21c5d448
raokaif <kaifrao250@gmail.com>
2023-12-20 22:54:45 +0500

    1st function

--- array_utils.c
+++ array_utils.c
@@ -1,3 +1,17 @@
 
 
-//place your function definitions here
+// place your function definitions here
+int contains(const int *arr, int size, int x)
+{
+    for (int i = 0; i < size; i++, arr++)
+    {
+        if (*arr == x)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
