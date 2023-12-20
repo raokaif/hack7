d3f196ba8a3caed9f6626d11edcbd3347a92d800
raokaif <kaifrao250@gmail.com>
2023-12-21 01:22:27 +0500

    adding all prototypes in .h file

--- array_utils.h
+++ array_utils.h
@@ -1,3 +1,8 @@
 
 
 //place your documentation and prototypes here
+int contains(const int *arr, int size, int x);
+int containsWithin(const int *arr, int size, int x, int i, int j);
+int * paddedCopy(const int *arr, int oldSize, int newSize);
+void reverse(int *arr, int size);
+int * reverseCopy(const int *arr, int size);
